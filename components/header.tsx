e171dcdf7e8495d22d129b0d457aab2d4dc855e8
Kyaw Thu <evanch98@gmail.com>
2024-04-24 21:19:55 +0800

    Update the Header component to finalize the Logo part

--- components/header.tsx
+++ components/header.tsx
@@ -5,9 +5,12 @@ export const Header = () => {
     <header className="w-full text-gray-100 bg-gray-950">
       <div className="max-w-screen-lg mx-auto flex items-center justify-between py-4">
         <nav className="flex space-x-16 items-center">
+          {/* Logo - Start */}
           <div className="flex space-x-1 items-center">
             <Globe2 className="h-8 w-8" />
+            <p className="text-xl font-bold">Earth</p>
           </div>
+          {/* Logo - End */}
         </nav>
       </div>
     </header>
