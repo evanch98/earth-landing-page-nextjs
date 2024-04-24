dd1e93434bf12329f72796b997c18115064a6370
Kyaw Thu <evanch98@gmail.com>
2024-04-24 21:13:52 +0800

    Update the Header component to structure the layout and add Globe2 Icon

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,13 @@
+import { Globe2 } from "lucide-react";
+
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between max-w-screen-lg mx-auto py-4">
-      Header
+    <header className="flex items-center justify-between max-w-screen-lg mx-auto py-4 text-gray-100">
+      <nav className="flex space-x-16 items-center">
+        <div className="flex space-x-1 items-center">
+          <Globe2 className="h-8 w-8" />
+        </div>
+      </nav>
     </header>
   );
 };
