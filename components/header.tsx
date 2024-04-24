826ffb050e21662ffae18f051705092472a936fd
Kyaw Thu <evanch98@gmail.com>
2024-04-24 21:18:13 +0800

    Update the Header component to restructure the layout

--- components/header.tsx
+++ components/header.tsx
@@ -2,12 +2,14 @@ import { Globe2 } from "lucide-react";
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between max-w-screen-lg mx-auto py-4 text-gray-100">
-      <nav className="flex space-x-16 items-center">
-        <div className="flex space-x-1 items-center">
-          <Globe2 className="h-8 w-8" />
-        </div>
-      </nav>
+    <header className="w-full text-gray-100 bg-gray-950">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-between py-4">
+        <nav className="flex space-x-16 items-center">
+          <div className="flex space-x-1 items-center">
+            <Globe2 className="h-8 w-8" />
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
